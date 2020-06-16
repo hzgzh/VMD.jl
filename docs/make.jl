58d117794ea709c5a7a1b8c89e6f010ac1ee4be2
hzgzh <hzgzh@sina.com>
2020-06-16 18:16:46 +0800

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,8 @@ makedocs(
     sitename="VMD.jl",
     pages=Any[
         "Home" => "index.md",
-        "Examples" => Dict("example=>""examples.md",
+        "Examples" => Dict(
+        "example"  => "examples.md",
         "example1" => "example1.md",
         "example2" => "example2.md",
         "exmaple3" => "example3.md"),
