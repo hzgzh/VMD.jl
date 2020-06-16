f2b42cde666c04203cd2ce0b0d51c9417edf2262
hzgzh <hzgzh@sina.com>
2020-06-16 12:40:45 +0800

    update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -6,7 +6,10 @@ makedocs(
     sitename="VMD.jl",
     pages=Any[
         "Home" => "index.md",
-        "Examples" => "examples.md",
+        "Examples" => Dict("example=>""examples.md",
+        "example1" => "example1.md",
+        "example2" => "example2.md"
+        "exmaple3" => "example3.md"),
         "Reference" => "api.md"
     ],
 )
